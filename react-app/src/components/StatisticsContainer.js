82efe8d41aa5fe5723b1edd7d7bd46b727fb4b94
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2017-07-16 20:59:56 +0200

    Add StatisticsContainer component

---
+++ react-app/src/components/StatisticsContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class StatisticsContainer extends Component {
+  render() {
+    return (
+      <div className='Statistics-container'>
+        Statistics
+      </div>
+    );
+  }
+}
+
+export default StatisticsContainer;
