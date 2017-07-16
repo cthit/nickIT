1c19bf3740fa2d5fc3f82d8a0693f355774e133e
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2017-07-16 21:03:40 +0200

    Add constructor and Components InfoBody should contain

--- react-app/src/components/InfoBody.js
+++ react-app/src/components/InfoBody.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import '../App.css';
+import NickList from './NickList';
+import StatisticsContainer from './StatisticsContainer';
 
 class InfoBody extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      nick_list: []
+    };
+  }
+
   render() {
     return(
       <div className="Info-body">
-
+        <NickList nick_list={this.props.nick_list}/>
+        <StatisticsContainer />
       </div>
     );
   }
