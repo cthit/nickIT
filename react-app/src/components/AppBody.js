ee0819f54e8fc3bf1e3589076ec544284077011c
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2017-07-16 21:09:27 +0200

    Add a way for the SearchBar to send the list to the InfoBody

--- react-app/src/components/AppBody.js
+++ react-app/src/components/AppBody.js
@@ -4,13 +4,27 @@ import SearchBar from './SearchBar.js';
 import InfoBody from './InfoBody.js';
 
 class AppBody extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      nick_list: []
+    };
+  }
+
   render() {
     return (
       <div className="App-body">
-        <SearchBar />
-        <InfoBody />
+        <SearchBar onSearch={this.handleSearch.bind(this)}/>
+        <InfoBody nick_list={this.state.nick_list}/>
       </div>
     );
+  }
+
+  handleSearch(list) {
+    this.setState({
+      nick_list: list
+    });
   }
 }
 
