7c23fa43fff4994ade3b2a67bcf2a9d58254e1bc
Joakim Hulthe <joakim@hulthe.net>
2017-07-24 16:00:40 +0200

    Add CORS headers to backend requests

--- rocket-app/src/main.rs
+++ rocket-app/src/main.rs
@@ -3,6 +3,7 @@
 
 mod ldap_hook;
 mod endpoints;
+mod util;
 
 extern crate rocket;
 extern crate rocket_contrib;
@@ -10,11 +11,14 @@ extern crate ldap3;
 #[macro_use]
 extern crate serde_derive;
 
+use util::cors::CORS;
+
 fn main() {
 	rocket::ignite().mount("/", routes![
 		endpoints::search::empty_search_nick,
 		endpoints::search::search_nick,
 		endpoints::search::empty_search_uid,
 		endpoints::search::search_uid,
-	]).launch();
+	]).attach(CORS())
+	  .launch();
 }
