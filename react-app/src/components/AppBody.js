99792097ee1b72d0b4206f562fbf3614c2508fd2
Anton Levholm <anton.badminton@gmail.com>
2017-08-24 10:48:39 +0200

    some refactoring

--- react-app/src/components/AppBody.js
+++ react-app/src/components/AppBody.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import '../App.css';
-import SearchBar from './SearchBar.js';
-import InfoBody from './InfoBody.js';
+import React, { Component } from "react";
+import "../App.css";
+import SearchBar from "./SearchBar.js";
+import NickList from "./NickList";
+import StatisticsContainer from "./StatisticsContainer";
 
 class AppBody extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -13,10 +13,16 @@ class AppBody extends Component {
   }
 
   render() {
+    const { nick_list: NICK_LIST } = this.state;
+    const handleSearch = this.handleSearch.bind(this);
+
     return (
-      <div className="App-body">
-        <SearchBar onSearch={this.handleSearch.bind(this)}/>
-        <InfoBody nick_list={this.state.nick_list}/>
+      <div className="app-body">
+        <SearchBar onSearch={handleSearch} />
+        <main className="info-body">
+          <NickList nick_list={NICK_LIST} />
+          <StatisticsContainer />
+        </main>
       </div>
     );
   }
